452b2f8a4c9c59d2189666c2b08d569ecadca21b
unknown <tigerhoodUday>
2023-09-18 11:06:42 +0530

    9/18/2023__11:06

---
+++ External Storage Class/support.cpp
@@ -0,0 +1,13 @@
+//Here, extern is being used to declare count in the second file, where as it has its definition in the first file, main.c. Now, compile these two files as follows
+
+// g++ main.cpp support.cpp
+// .\a.exe
+
+
+#include <stdio.h>
+ 
+extern int count;
+ 
+void write_extern(void) {
+   printf("count is %d\n", count+1);
+}
